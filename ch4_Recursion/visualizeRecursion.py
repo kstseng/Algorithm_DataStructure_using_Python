54cf7fd3941fd0d69e87d889695e5d072dc276bf
davidtseng <davidtseng@kkbox.com>
2018-02-05 22:42:15 +0800

    visualizeRecursion

---
+++ ch4_Recursion/visualizeRecursion.py
@@ -0,0 +1,36 @@
+import turtle
+
+myTurtle = turtle.Turtle()
+myWin = turtle.Screen()
+
+def drawSpiral(myTurtle, lineLen):
+    if lineLen > 0:
+        myTurtle.forward(lineLen)
+        myTurtle.right(85)
+        drawSpiral(myTurtle, lineLen - 5)
+
+drawSpiral(myTurtle, 150)
+myWin.exitonclick()
+
+def tree(branchLen, t):
+    if branchLen > 1:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen - 15, t)
+        t.left(40)
+        tree(branchLen - 15, t)
+        t.right(20)
+        t.backward(branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color('green')
+    tree(75, t)
+    myWin.exitonclick()
+
+main()

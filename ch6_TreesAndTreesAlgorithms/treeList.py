d2b022a0e00b3cb04db3e306c747e955652d2de9
davidtseng <davidtseng@kkbox.com>
2018-04-22 16:19:22 +0800

    build tree by list of list

---
+++ ch6_TreesAndTreesAlgorithms/treeList.py
@@ -0,0 +1,56 @@
+# build tree functions
+def BinaryTree(r):
+    return [r, [], []]
+
+def insertLeft(root, newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1, [newBranch, t, []])
+    else:
+        root.insert(1, [newBranch, [], []])
+    return root
+
+def insertRight(root, newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2, [newBranch, [], t])
+    else:
+        root.insert(2, [newBranch, [], []])
+    return root
+
+
+# access functions
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root, newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+# 
+r = BinaryTree(3)
+insertLeft(r, 4)
+insertLeft(r, 5)
+insertRight(r, 6)
+insertRight(r, 7)
+l = getLeftChild(r)
+print(l)
+
+setRootVal(l, 9)
+print(r)
+insertLeft(l, 11)
+print(r)
+print(getRightChild(getRightChild(r)))
+
+## self Check 2
+x = BinaryTree('a')
+insertLeft(x, 'b')
+insertRight(getLeftChild(x), 'd')
+insertRight(x, 'c')
+insertLeft(getRightChild(x), 'e')
+insertRight(getRightChild(x), 'f')
